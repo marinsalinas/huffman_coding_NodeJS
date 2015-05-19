94657d9cfbaa8060ea5b4eff9643930ed218007f
Marín Salinas <marinssalinas@gmail.com>
2015-05-19 18:37:25 -0500

    Make Huffman Tree Structure

--- index.js
+++ index.js
@@ -22,25 +22,56 @@ var huffman = (function(){
       })
     }
     /**
-      Metodo: getLeafs nos sirve para obtener un arreglo de 'diccionarios' que cada diccionario representa un elemento de caracter referencia;
-      Se
+      Metodo: "getLeafs" nos sirve para obtener un Array de 'diccionarios'
+              cada elemento del array es un objeto con la estructura {num:num, key:key}
+              el cual nos ayuda a tener una estructura mas organizada de cada 'nodo'
+              ademas de utilizarlo para obetener funciones básicas de ir array como lenght
+              y evitarnos refactorizar.
     */
     function getLeafs(frequencies){
       return _.map(frequencies, function(num, key){
         return {num:num, key:key}
       })
     }
+    /**
+      Metodo: "getQueue", nos asegura de que este ordenado nuestro Array de elementos o nodos, de mayor a menor
+      que nos facilita el procedimiento de huffman.
+    */
     function getQueue(leafs){
-      return _.sortBy(leafs, function(tuple){
-        return tuple.num
-      })
+      return _.sortBy(leafs, function (elem) {
+		            return elem.num;
+              }).reverse();
+    }
+    /*Metodo: "getInternalNode" es donde comenzamos a crear la estructura de arbol
+      La cual nos va a ayudar a ir encadenando cada nodo.
+    */
+    function getInternalNode(node0, node1){
+      var sum = node0.num + node1.num
+      var node = {num: sum, childNode0: node0, childNode1:node1}
+      node0.parent = node
+      node1.parent = node
+      return node
     }
 
+    function addNode(queue, newNode){
+      queue.push(newNode)
+      tempQ = getQueue(queue)
+      queue = tempQ;
+    }
 
     self.encode = function(text){
       var frequencies = getFrecuencies(text)
       var leafs = getLeafs(frequencies)
-      var queue = getQueue(leafs)      
+      var queue = getQueue(leafs)
+
+
+      //Este procedimiento se repite hasta que todos los nodos esten unidos
+      while(_.size(queue) > 1){
+        var node0 = queue.pop()
+        var node1 = queue.pop()
+        var newNode = getInternalNode(node0, node1);
+        addNode(queue, newNode);
+      }
       return queue;
     }
 
